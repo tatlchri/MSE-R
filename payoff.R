ae197ceea257f1ccd9ccb0bb2b297ba8b07d86e2
tatlchri <christinatatli@gmail.com>
2017-03-29 12:52:55 +0100

    payoff function

---
+++ payoff.R
@@ -0,0 +1,34 @@
+Cx<-function(n){
+  #Cx(n) creates a list of n variables named x1,x2,...,xn.
+  
+  Cx<-paste("x",seq( from = 1, to = n, by = 1 ),sep="")
+  
+  return(Cx)
+}
+
+
+payoffDM<-function(Cx,distanceMatrices,noAttr,m,i,j){
+  #payoffDM(Cx,distanceMatrices,noAttr,m,i,j) returns the payoff of i-upstream and j-upstream in the m-market
+  
+  x<-as.numeric((distanceMatrices[[m]][i][[1]][j,]))*Sym(c(1,Cx[1:noAttr-1]))
+  y<-""
+  for(i in 1:noAttr){
+    y<-y+Sym(x[i])
+  }
+  
+  return(y)
+  
+}
+
+CpayoffMatrix<-function(noM,noU,noD,Cx,distanceMatrices,noAttr){
+  #payoffMatrix=CpayoffMatrix(noM,noU,noD,Cx,distanceMatrices,noAttr) calculates and assigns the payoffMatrix 
+  
+  CpayoffMatrix<-rep(list(list()),noM)
+  CpayoffMatrix<-lapply(seq_along(CpayoffMatrix),function(i) rep(list(rep(list(list()),noD[[i]])),noU[[i]]))
+  CpayoffMatrix<-lapply(seq_along(CpayoffMatrix), function(i) lapply(seq_along(CpayoffMatrix[[i]]),function(j) lapply(seq_along(CpayoffMatrix[[i]][[j]]),function(k) payoffDM(Cx,distanceMatrices,noAttr,i,j,k))))
+  return(CpayoffMatrix)
+}
+
+
+
+
