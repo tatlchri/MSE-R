4511f0c504f022d325ac1537316966b87a54074d
tatlchri <christinatatli@gmail.com>
2017-03-29 14:24:37 +0100

    mse file

---
+++ mse.R
@@ -0,0 +1,21 @@
+#Install required packages
+if (!"Matrix" %in% installed.packages()) install.packages("Matrix")
+if (!"lpSolve" %in% installed.packages()) install.packages("lpSolve")
+if (!"dplyr" %in% installed.packages()) install.packages("dplyr")
+if (!"Ryacas" %in% installed.packages()) install.packages("Ryacas")
+if (!"doSNOW" %in% installed.packages()) install.packages("doSNOW")
+if (!"foreach" %in% installed.packages()) install.packages("foreach")
+
+#Import installed packages
+library(Matrix)
+library(lpSolve)
+library(dplyr)
+library(Ryacas)
+library(doSNOW)
+library(foreach)
+
+#Import functions
+source("import.R")
+source("assignpayoffMatrix.R")
+source("matching.R")
+source("payoff.R")
