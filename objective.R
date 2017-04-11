7424cb9a12d549800a8c4bf86d8494096aacd9b8
tatlchri <christinatatli@gmail.com>
2017-04-11 14:33:47 +0100

    objective function

---
+++ objective.R
@@ -0,0 +1,5 @@
+objective<-function(b){
+  #objective[x1,x2,...,xn] defines the objective function to minimize, as the number of satisfied inequalities. For a specific x-vector value we get a list of numbers. The number of positives is the outcome.
+  return(-(sum((matrix(unlist(dataArray,use.names=FALSE),nrow=length(dataArray),byrow=TRUE)%*%c(coefficient1,b) )>0)))
+}
+  
