4fb45030a0427b4a19cd56551ad2f85ca6121a9e
tatlchri <christinatatli@gmail.com>
2017-04-17 14:02:54 +0100

    function to assign list vlaues

---
+++ assignList.R
@@ -0,0 +1,24 @@
+# Generic form
+'%=%' = function(l, r, ...) UseMethod('%=%')
+
+'%=%.lbunch' = function(l, r, ...) {
+  Envir = as.environment(-1)
+  
+  if (length(r) > length(l))
+    warning("RHS has more args than LHS. Only first", length(l), "used.")
+  
+  if (length(l) > length(r))  {
+    warning("LHS has more args than RHS. RHS will be repeated.")
+    r <- extendToMatch(r, l)
+  }
+  
+  for (II in 1:length(l)) {
+    do.call('<-', list(l[[II]], r[[II]]), envir=Envir)
+  }
+}
+
+g = function(...) {
+  List = as.list(substitute(list(...)))[-1L]
+  class(List) = 'lbunch'
+  return(List)
+}
